bf5989caba9105df2d2fa373467955fea090c87e
kanu mohindra <kanu.32@hotmail.com>
2017-12-31 22:04:36 -0500

    new with online services

---
+++ SureSafetyMVC/Controllers/EmployeeDetailsController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using SureSafetyDB;
+
+namespace SureSafetyMVC.Controllers
+{
+    public class EmployeeDetailsController : ApiController
+    {
+        private SureSafetyDBModelContainer db = new SureSafetyDBModelContainer();
+
+        // GET: api/EmployeeDetails
+        public IQueryable<EmployeeDetails> GetEmployeeDetails()
+        {
+            return db.EmployeeDetails;
+        }
+
+        // GET: api/EmployeeDetails/5
+        [ResponseType(typeof(EmployeeDetails))]
+        public IHttpActionResult GetEmployeeDetails(int id)
+        {
+            EmployeeDetails employeeDetails = db.EmployeeDetails.Find(id);
+            if (employeeDetails == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(employeeDetails);
+        }
+
+        // PUT: api/EmployeeDetails/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutEmployeeDetails(int id, EmployeeDetails employeeDetails)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != employeeDetails.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(employeeDetails).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EmployeeDetailsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/EmployeeDetails
+        [ResponseType(typeof(EmployeeDetails))]
+        public IHttpActionResult PostEmployeeDetails(EmployeeDetails employeeDetails)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.EmployeeDetails.Add(employeeDetails);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = employeeDetails.Id }, employeeDetails);
+        }
+
+        // DELETE: api/EmployeeDetails/5
+        [ResponseType(typeof(EmployeeDetails))]
+        public IHttpActionResult DeleteEmployeeDetails(int id)
+        {
+            EmployeeDetails employeeDetails = db.EmployeeDetails.Find(id);
+            if (employeeDetails == null)
+            {
+                return NotFound();
+            }
+
+            db.EmployeeDetails.Remove(employeeDetails);
+            db.SaveChanges();
+
+            return Ok(employeeDetails);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool EmployeeDetailsExists(int id)
+        {
+            return db.EmployeeDetails.Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ SureSafetyMVC/Controllers/IncidentDetailsController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using SureSafetyDB;
+
+namespace SureSafetyMVC.Controllers
+{
+    public class IncidentDetailsController : ApiController
+    {
+        private SureSafetyDBModelContainer db = new SureSafetyDBModelContainer();
+
+        // GET: api/IncidentDetails
+        public IQueryable<IncidentDetails> GetIncidentDetails()
+        {
+            return db.IncidentDetails;
+        }
+
+        // GET: api/IncidentDetails/5
+        [ResponseType(typeof(IncidentDetails))]
+        public IHttpActionResult GetIncidentDetails(int id)
+        {
+            IncidentDetails incidentDetails = db.IncidentDetails.Find(id);
+            if (incidentDetails == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(incidentDetails);
+        }
+
+        // PUT: api/IncidentDetails/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutIncidentDetails(int id, IncidentDetails incidentDetails)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != incidentDetails.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(incidentDetails).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!IncidentDetailsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/IncidentDetails
+        [ResponseType(typeof(IncidentDetails))]
+        public IHttpActionResult PostIncidentDetails(IncidentDetails incidentDetails)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.IncidentDetails.Add(incidentDetails);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = incidentDetails.Id }, incidentDetails);
+        }
+
+        // DELETE: api/IncidentDetails/5
+        [ResponseType(typeof(IncidentDetails))]
+        public IHttpActionResult DeleteIncidentDetails(int id)
+        {
+            IncidentDetails incidentDetails = db.IncidentDetails.Find(id);
+            if (incidentDetails == null)
+            {
+                return NotFound();
+            }
+
+            db.IncidentDetails.Remove(incidentDetails);
+            db.SaveChanges();
+
+            return Ok(incidentDetails);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool IncidentDetailsExists(int id)
+        {
+            return db.IncidentDetails.Count(e => e.Id == id) > 0;
+        }
+    }
+}
